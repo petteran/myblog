83b204157a0a0c0719fa1b20940466cb2764e1da
petteran <petteran.h@qq.com>
2021-03-19 04:47:36 +0800

    mybatisplus 测试 controller

--- src/main/java/com/petter/myblog/controller/UserController.java
+++ src/main/java/com/petter/myblog/controller/UserController.java
@@ -1,6 +1,11 @@
 package com.petter.myblog.controller;
 
 
+import com.petter.myblog.entity.User;
+import com.petter.myblog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +22,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
-    public User
+    @Autowired
+    UserService userService;
+    @GetMapping("{id}")
+    public Object getUser(@PathVariable("id") Long id) {
+        return userService.getById(id);
+    }
 
 
 }
